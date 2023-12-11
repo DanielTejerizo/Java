e826d7eebee7671e50e17221129a279e13225267
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-12-11 18:06:16 +0100

    .

--- Calculadora/src/java/Calculadora.java
+++ Calculadora/src/java/Calculadora.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -10,91 +5,68 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author DAW
- */
 public class Calculadora extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<h1>Servlet Calculadora at " + request.getContextPath() + "</h1>");
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Calculadora</title>");
-            out.println("<table border=1>");
-            
-            int resultado=0;
-            
-            for(int i=1; i<11; i++){
-            
-            out.println("<tr>");
-             for (int j=1;j<11;j++){
-                 out.println("<td>");
-                 resultado=j*i;
-                 out.println(resultado);
-                 resultado=0;
-             }
+        try (PrintWriter out = response.getWriter()) {
+
+            String num1S = request.getParameter("num1");
+            String num2S = request.getParameter("num2");
+            String operacion = request.getParameter("operacion");
+
+
+            int num1 = Integer.parseInt(num1S);
+            int num2 = Integer.parseInt(num2S);
+            int resultado = 0;
+
+            switch (operacion) {
+                case "sumar":
+                    resultado = num1 + num2;
+                    break;
+                case "restar":
+                    resultado = num1 - num2;
+                    break;
+                case "multiplicar":
+                    resultado = num1 * num2;
+                    break;
+                case "dividir":
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else {
+                        out.println("Error: Division por 0.");
+                        return;
+                    }
+                    break;
+                default:
+                    out.println("Error");
+                    return;
             }
-            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("</body>");
-            out.println("</html>");
+
+
+            out.println("<h1>El resultado de la  " + operacion + " es:</h1>");
+            out.println("<p>" + num1 + " " + operacion + " " + num2 + " = " + resultado + "</p>");
+        } catch (NumberFormatException e) {
+
+            response.getWriter().println("Error.");
         }
     }
 
- 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        return "Servlet for basic calculator operations";
+    }
 }
